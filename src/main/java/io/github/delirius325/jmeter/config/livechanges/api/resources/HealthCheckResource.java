f58a0d68c7e7fc5ccaf74f31b59a797a4519543a
Anthony Gauthier <antho325@hotmail.com>
2020-08-27 15:13:54 -0400

    WIP #21 - Added swagger and swagger UI for GitHub pages

---
+++ src/main/java/io/github/delirius325/jmeter/config/livechanges/api/resources/HealthCheckResource.java
@@ -0,0 +1,23 @@
+package io.github.delirius325.jmeter.config.livechanges.api.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Base endpoint for the ConnectivityResource class
+ */
+@Path("/healthcheck")
+public class HealthCheckResource {
+    /**
+     * A simple endpoint testing the connectivity to the API
+     * @return returns the string "connected" if successful
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getConnected() {
+        return "connected";
+    }
+}
